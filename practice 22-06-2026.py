2a41c690d2cf806e1a65d5c475173643a930fd5a
MuhammadHaroonjaffar-oss <mharoonjaffar@gmail.com>
2026-06-22 18:44:38 +0500

    practice22-06-2026

---
+++ practice 22-06-2026.py
@@ -0,0 +1,23 @@
+Property_Id = 784
+print(Property_Id)
+print(type(Property_Id))
+Property_price = 50000.5
+print(Property_price)
+print(type(Property_price))
+
+property_area = "Gullburge"
+print(property_area)
+print(type(property_area)) 
+Property_price = Property_price * 5
+print(Property_price)
+print(type(Property_price))
+property_City=input("Enter city")
+print(property_City)
+print(type(property_City))
+property_info=input("enter property info")
+print(property_info)
+print(len(property_info))
+for i in property_info:
+    print(i)
+print(property_info[2:4:1])
+print("End program")
